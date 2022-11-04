15213591bb30f9b41c643893db7c2ef866ab2f66
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-11-04 22:56:30 +0600

    server setup and load data from db functionality added

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mfyq6m8.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const productCollection = client.db('emaJohn').collection('products');
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.errpr(error));
+
+
+
+app.get('/', (req, res) => {
+    res.send('ema-john-server is running');
+});
+
+app.listen(port, () => {
+    console.log(`ema john server is running on port: ${port}`);
+});
